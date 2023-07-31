38c930ca2475983a44109acb02a80227cc6a3b25
Daniel Ribeiro <daniel.ribeiro@pathzero.com>
2023-07-31 10:36:04 +1000

    Make seeds idempotent

--- backend/app/initial_data.py
+++ backend/app/initial_data.py
@@ -1,26 +1,29 @@
 #!/usr/bin/env python3
 
-from app.db.session import get_db
-from app.db.user_crud import create_user
-from app.db.schemas import UserCreate
+from app.db.user_crud import create_user, get_user_by_email
+from app.db.user_schemas import UserCreate
 from app.db.session import SessionLocal
 
 
 def init() -> None:
     db = SessionLocal()
 
-    create_user(
-        db,
-        UserCreate(
-            email="",
-            password="",
-            is_active=True,
-            is_superuser=True,
-        ),
-    )
+    admin_email = ""
+    user = get_user_by_email(db, admin_email)
+
+    if not user:
+        print("Creating superuser")
+        create_user(
+            db,
+            UserCreate(
+                email="",
+                password="",
+                is_active=True,
+                is_superuser=True,
+            ),
+        )
+        print("Superuser created")
 
 
 if __name__ == "__main__":
-    print("Creating superuser")
     init()
-    print("Superuser created")
